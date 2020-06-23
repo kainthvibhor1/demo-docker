ad555bc9e14a45ca4440cea3dc36c3bd2dc643dc
Vibhor Kainth <vibhor.kainth@perficient.com>
2020-06-23 12:59:13 +0530

    Feature: Externalize the Tomcat server

--- src/main/java/pgt/goal/MainApplication.java
+++ src/main/java/pgt/goal/MainApplication.java
@@ -2,12 +2,19 @@ package pgt.goal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MainApplication.class);
 	}
 
 }
